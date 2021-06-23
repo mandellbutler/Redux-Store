76a001126dc54e9a15d79280e6d000ccde22f189
Mandell Butler <mandell.butler@outlook.com>
2021-06-23 19:45:32 -0400

    Connected global state data from redux store

--- client/src/index.js
+++ client/src/index.js
@@ -4,13 +4,15 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux';
-import store from '..'
+//bring in store for provider use
+import store from './context/store'
 
 ReactDOM.render(
-  <Provider store=>
+  //providing global state data via redux store
+  <Provider store={store}>
     <App />
-  </Provider >,
-  document.getElementById('root')
+  </Provider >
+  , document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
